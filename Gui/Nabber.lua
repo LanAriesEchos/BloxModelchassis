9f3b5733ed7aaa37ed2e47886487d3362c751157
LanAriesEchos <91972966+LanAriesEchos@users.noreply.github.com>
2022-05-29 21:00:56 -0400

    Add files via upload

---
+++ Gui/Nabber.lua
@@ -0,0 +1,4 @@
+script.Parent.MainBusFolders.Value = script.Parent.Parent
+script.Parent.BusFolder.Value = script.Parent.Parent.Parent
+script.Parent.MainBusFolders.Parent = script.Parent.RunHeader.ClientBusStorage
+script.Parent.MainBusFolders.Parent = script.Parent.RunHeader.ClientBusStorage
